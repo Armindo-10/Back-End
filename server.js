7f5c4545fbdb9c24c1abd223e88bfd48a8942bd3
Jenilson Francisco <franciscoviwaldo@gmail.com>
2025-06-08 13:24:30 +0100

    refactor: moding some files on the project

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import testRoutes from './routes/test.js';
+import { app } from './src/app.js';
 
-const PORT = process.env.PORT || 3000; // definindo porta do projeto
+const PORT = process.env.PORT || 3000;
 
-dotenv.config();
-const app = express();
 app.use(express.json());
 
-app.use('/', testRoutes); // Agora acessível em /
-
-//escutando a porta 3000
 app.listen(PORT, () => {
     console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
 })
